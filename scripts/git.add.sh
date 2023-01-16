b068788d5c7d0a6b23dfcd724f62085f5c6ebf2d
stephane <stephane@paperlib.com>
2023-01-16 13:54:21 +0100

    cleanup

--- scripts/git.add.sh
+++ scripts/git.add.sh
@@ -4,7 +4,7 @@ FILE=`realpath $1`
 
 # -- to "add" a file over on git's "staging" area
 # -- we need to first add it to its object database (ie. gits "objects" directory)
-# -- * for mode details see here:
+# -- -> for more details see here:
 # -- https://git-scm.com/book/en/v2/Git-Internals-Git-Objects#_object_storage
 # -- https://stackoverflow.com/questions/5290444/why-does-git-hash-object-return-a-different-hash-than-openssl-sha1
 filehash=`git hash-object -w $FILE`
